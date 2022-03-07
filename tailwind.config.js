a304788889365d45b937ce7e778b78ff019de841
BoltUltra <75336728+BoltUltra@users.noreply.github.com>
2022-03-07 11:06:01 +0000

    Edited the config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./index.html", "./index.js"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "320px",
+      // => @media (min-width: 320px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      fontFamily: {
+        ubuntu: ["Ubuntu", "sans-serif"],
+      },
+      colors: {
+        background: "#EFF0F3",
+        button: "#BA723D",
+        lightbg: "#FDEBDD",
+        paragraph: "#2A2A2A",
+        headline: "#0D0D0D",
+      },
+    },
   },
   plugins: [],
 };
